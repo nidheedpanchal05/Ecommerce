695006ed454e561434e676013612352d44b87ed4
nidheedpanchal05 <60962788+nidheedpanchal05@users.noreply.github.com>
2021-11-25 10:03:36 +0530

    Update settings.py

--- Backend/ecommerce/ecommerce/settings.py
+++ Backend/ecommerce/ecommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-e*c5+idosg#cs5u-1oxtje4fs8o&&=#8gvbbmibhg0qanxyh7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
